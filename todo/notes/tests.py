17acf932f4c31e1bf22fb4f04f4ffa573e30c5a6
apterek <danialsud@gmail.com>
2021-09-13 11:52:53 +0300

    up

--- todo/notes/tests.py
+++ todo/notes/tests.py
@@ -1,7 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from notes.models import Note
+from notes.views import index
 
 
 class Testnotes(TestCase):
-    def test_create_note(self) -> None:
+    def setUp(self) -> None:
+        self.factory = RequestFactory()
         self.notes = Note.objects.create(title="test", text="test_text")
+
+    def test_maker(self):
+        request = self.factory.get("")
+        responce = index(request)
+        self.assertEqual(responce.status_code, 200)
+        self.assertEqual(self.notes.pk, 1)
+
